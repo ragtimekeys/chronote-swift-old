ba8d94d3602cbe20e984795041bb426c624c02af
Christian Howe <cjhowe7@pm.me>
2018-07-01 21:22:27 -0400

    Add some examples for a few UIViews

--- Chronote/ViewController.swift
+++ Chronote/ViewController.swift
@@ -12,17 +12,50 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
 
         view.backgroundColor = .white
-        let myLabel = UILabel(frame: CGRect(x: 30, y: 20, width: 100, height: 40))
+
+        let myLabel = UILabel()
+        myLabel.textAlignment = .center
+        myLabel.backgroundColor = .red
         myLabel.text = "chronote sucks"
-        view.addSubview(myLabel)
+        myLabel.layer.cornerRadius = 50.0
+        myLabel.clipsToBounds = true
+
+        let myTextField = UITextField()
+        myTextField.placeholder = "Text input"
+
+        let mySegmentedControl = UISegmentedControl(items: [
+            "A",
+            "B"
+        ])
+
+        let myDatePicker = UIDatePicker()
+        myDatePicker.datePickerMode = .countDownTimer
+        myDatePicker.backgroundColor = .blue
+
+        let stackView = UIStackView(arrangedSubviews: [
+            myLabel,
+            myTextField,
+            mySegmentedControl,
+            myDatePicker
+        ])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
